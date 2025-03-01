a53631b93632f0e44dad9e60af0ea63b92f87d62
inder3003KAUR <inder3003@gmail.com>
2025-03-01 10:13:33 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// You can add interactivity to your site here in the future.
+// Example: Alert when a user visits the site
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Welcome to Dr. Inderpreet Kaur's Professional Profile");
+});
